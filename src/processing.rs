b7ffbe43b60473dfaee5adec9af9c96f5d5ade51
artemiksion <artemiksion@mail.ru>
2022-09-25 13:38:16 +0300

    added parallel orders parsing

--- src/processing.rs
+++ src/processing.rs
@@ -8,24 +8,29 @@ use crate::primitives::{
 use std::fs::read_to_string;
 use threadpool::ThreadPool;
 use num_cpus;
+use rayon::prelude::*;
 use crate::transactions::transaction;
 
 pub fn parsing_orders(sender: Sender<Order>) {
     let file = read_to_string("Orders.txt").expect("Without this file we cann't do orders");
-    for i in file.split("\n") {
-        let mut temp: Vec<String> = Vec::new();
-        for j in i.split("\t") {
-            temp.push(j.to_string());
-        }
-        sender.send(Order {
-            client: temp[0].clone(),
-            action_with_order: temp[1].chars().next().expect("Incorrectly entered input data"),
-            currency: temp[2].clone(),
-            price: temp[3].parse().expect("Incorrectly entered input data"),
-            amount: temp[4].parse().expect("Incorrectly entered input data"),
-        }).expect("Closed channel");
-
-    }
+    let vec_of_slice: Vec<&str> = file.split("\n").collect();
+    let vec_of_slice_len = vec_of_slice.len();
+    vec_of_slice.par_chunks(vec_of_slice_len).for_each_with(sender, |sender, chunk| {
+        chunk.into_iter().for_each(|row| {
+            let mut temp: Vec<String> = Vec::new();
+            row.split("\t").into_iter().for_each(|item| {
+                temp.push(item.to_string());
+            });
+            sender.send(Order {
+                client: temp[0].clone(),
+                action_with_order: temp[1].chars().next().expect("Incorrectly entered input data"),
+                currency: temp[2].clone(),
+                price: temp[3].parse().expect("Incorrectly entered input data"),
+                amount: temp[4].parse().expect("Incorrectly entered input data"),
+            }).expect("Closed channel");
+        });
+        
+    });
 }
 
 pub fn order_processing(receiver_parsing_order: Receiver<Order>, sender_succes_order: Sender<SuccessfulOrder>, order_books: &mut OrderBooks) {
